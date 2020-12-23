f0cd8d43b150e95ef7f5cf41169384dd552929dd
Rezan92 <rezanibrahim8@gmail.com>
2020-12-23 13:55:41 +0100

    Removed loading

--- src/components/CityWeatherContainer.js
+++ src/components/CityWeatherContainer.js
@@ -5,16 +5,15 @@ import SearchInput from "./SearchInput";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const CityWeatherContainer = () => {
-  const [cityName, setCityName] = useState("Amsterdam");
+  const [cityName, setCityName] = useState("");
   const [cityWeatherInfo, setCityWeatherInfo] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
   const [ifError, setIfError] = useState({});
 
+  const city = cityName.trim() !== "" ? cityName : "Amsterdam";
   useEffect(() => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     const fetchData = async () => {
       try {
-        setIsLoading(true);
         const response = await fetch(URL);
         const data = await response.json();
         if (data.cod >= 400) {
@@ -24,19 +23,16 @@ const CityWeatherContainer = () => {
         } else {
           setIfError({});
           setCityWeatherInfo(data);
-          setIsLoading(false);
         }
       } catch (error) {
         setIfError(error.data);
-        setIsLoading(false);
       }
     };
     fetchData();
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleSubmit = async () => {
     try {
-      setIsLoading(true);
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
       );
@@ -48,21 +44,15 @@ const CityWeatherContainer = () => {
       } else {
         setIfError({});
         setCityWeatherInfo(data);
-        setIsLoading(false);
       }
     } catch (error) {
       setIfError(error.data);
-      setIsLoading(false);
     }
   };
 
   return (
     <div className="city-weather-container">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : Object.keys(ifError).length > 0 ? (
-        <p>somthing went wrong</p>
-      ) : Object.keys(cityWeatherInfo).length > 0 ? (
+      {Object.keys(cityWeatherInfo).length > 0 ? (
         <>
           <SearchInput
             cityName={cityName}
